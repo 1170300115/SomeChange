524d6861a6705a16211754235434130c0fe7c69f
koyomi <1300990210@qq.com>
2019-07-24 00:49:16 +0800

    someChange

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+/*
+ * Small behaviour to handle menu button callbacks.
+ */
+public class MenuController : MonoBehaviour
+{
+    /*
+     * When the start button is pressed, load the Game scene.
+     */
+    public GameObject window;
+  public void OnStartClicked()
+  {
+    Application.LoadLevel("Level0");
+  }
+
+    public void OnstartClicked1()
+    {
+        Application.LoadLevel("Level1");
+    }
+
+    public void OnstartClicked2()
+    {
+        Application.LoadLevel("Level2");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+
+    /*
+     * When the back button is clicked, load the Menu scene.
+     */
+    public void OnBackClicked()
+  {
+    Application.LoadLevel("Menu");
+  }
+
+    public void OnInfoClicked()
+    {
+
+       Instantiate(window);
+    }
+
+    [System.Obsolete]
+    public void OnGameClicked()
+    {
+        Application.LoadLevel("LevelSence");
+    }
+    public void OnStopClicked()
+    {
+        Instantiate(window);
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/*
+ * Provide the obstacles with a way of damaging the player.
+ */
+public class PlayerCollider : MonoBehaviour
+{
+
+    public string thisLevel;
+
+    /*
+     * A trigger callback to detect when the player's collider has
+     * entered the obstacle's. Then simply obtain the PlayerController
+     * reference can apply damage. Then remove the obstacle for feedback.
+     */
+    private void OnTriggerEnter2D(Collider2D other)
+  {
+    // Obtain a reference to the Player's PlayerController
+    PlayerController playerController =
+      other.gameObject.GetComponent<PlayerController>();
+    
+        if (gameObject.tag == "fallDown")//坠崖死亡
+        {
+            playerController.Die();
+        }
+        else if (gameObject.tag == "badTerrain")//有害地形
+        {
+            playerController.Damage();
+        }
+        else if (gameObject.tag == "nextLevel") //下一关卡
+        {
+            Application.LoadLevel("Level" + thisLevel);
+        }
+        else if (gameObject.tag == "endofGame") //通关
+        {
+            Application.LoadLevel("AllClear");
+        }
+        else//普通伤害
+        {
+            // Register damage with player
+            playerController.Damage();
+
+            // Make this object disappear
+            GameObject.Destroy(gameObject);
+        }
+
+
+    
+  }
+}
